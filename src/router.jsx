81efd49daf81d3a203aab54af4ab1ca83e1113e7
fravila08 <fr4v1l4@gmail.com>
2025-12-12 15:25:27 -0800

    add url and pages to router

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,41 @@
 import { createHashRouter } from "react-router-dom";
-import App from "./App.jsx"
+import App from "./App";
 
-const router = createHashRouter([{
-    element: <App/>,
-    path:'/'
-}])
+import ErrorPage from "./pages/ErrorPage";
+import UserSelectPage from "./pages/UserSelectPage";
+import FilmDisplayPage from "./pages/FilmDisplayPage";
+import FilmDetailPage from "./pages/FilmDetailPage";
+import FilterDisplayPage from "./pages/FilterDisplayPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <UserSelectPage />,
+      },
+      {
+        path: "films",
+        element: <FilmDisplayPage />,
+      },
+      {
+        path: "films/:id",
+        element: <FilmDetailPage />,
+      },
+      {
+        path: "filter/:param",
+        element: <FilterDisplayPage />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+]);
 
 export default router;
